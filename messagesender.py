51e4612aa0696c0ca7faee6d134f2495be6e52a6
Chris WIthers <withscottc921@gmail.com>
2020-07-13 15:22:14 -0600

    Update messagesender.py

--- messagesender.py
+++ messagesender.py
@@ -12,12 +12,12 @@ for line in script:
     for x in line:
         script2.append(x)
 
-client = Client("elderwithers456@gmail.com", "password!")
+client = Client("username", "password!")
 users = client.fetchAllUsers()
 thread_type = ThreadType.USER
 thread_id = []
 for user in users:
-    if user.name == "Jake Withers":
+    if user.name == "users name you want to send message to":
         thread_id.append(user.uid)
 for _ in range(len(script2)-1):
     client.send(Message(text=f'{script2[message_count]}'), thread_id=int(thread_id[0]), thread_type=thread_type)
